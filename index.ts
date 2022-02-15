8b11fa84be61f377ee485a5354512335ee2f5ef1
Konstantin Mednikov <irondsd@gmail.com>
2022-02-15 11:03:43 +0300

    package

---
+++ index.ts
@@ -0,0 +1,11 @@
+import IntlProvider from './src/IntlProvider'
+import Message from './src/Message'
+import useIntl from './src/useIntl'
+import './types.d.ts'
+
+
+export {
+  IntlProvider,
+  Message,
+  useIntl,
+}
